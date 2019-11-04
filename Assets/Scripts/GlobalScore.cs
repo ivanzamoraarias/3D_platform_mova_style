5369df6a149477ca20ef9c362f3c0759a1270770
ivanzamoraarias <faucar93@gmail.com>
2019-11-04 02:05:50 +0100

    first commit

---
+++ Assets/Scripts/GlobalScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalScore : MonoBehaviour {
+
+    public GameObject scoreBox;
+    public static int currentScore;
+    public int internalScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        internalScore = currentScore;
+        scoreBox.GetComponent<Text>().text = "" + internalScore;
+    }
+}
